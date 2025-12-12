d0022f2574a9f63fd5e9db7fef301acbf58febfa
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2025-12-12 23:24:38 +0530

    Logest Common Prefix in Javascript

---
+++ js/longestCommonPrefix.js
@@ -0,0 +1,33 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+    let prefix = strs[0];   // assume first word is prefix
+    
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1); // cut last char
+            if (!prefix) return "";       // no prefix left
+        }
+    }
+    return prefix;
+}
