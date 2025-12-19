025d84d702b5dc932a871410856e452d7a9579dc
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2025-12-19 23:37:23 +0530

    Find Index of First Occurance In A String in Javascript

---
+++ js/findIndexOfFirstOccuranceInAStringJs.js
@@ -0,0 +1,15 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for(let i = 0; i <= haystack.length-needle.length; i++ ){
+        if(haystack.substring(i,i+needle.length)==needle){
+            return i;
+            break;
+        }
+    }
+    return -1;
+};
