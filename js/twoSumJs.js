dc3f78a49a3aac1f40aa3a0dd1ef122e1efb6c3f
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-02 16:49:13 +0530

    twoSum in Javascript

--- js/twoSumJs.js
+++ js/twoSumJs.js
@@ -4,19 +4,76 @@
 //Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    const map={};
-    for(let i=0;i< nums.length;i++){
-        const complement = target - nums[i];
-        if(map.hasOwnProperty(complement)){
-            return[map[complement],i];
+//NestedLoop O(N^2):
+/*
+- brute force approach
+- check every pair of numbers to see if they add up to the target
+- time complexity: O(N^2)
+- space complexity: O(1)
+*/
+
+function twoSumNL(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
-        map[nums[i]]=i;
     }
-    return[];
-};
+    return [];
+}
+
+//Two Pointers O(NlogN): 
+/*
+- sort the array and use two pointers to find the target sum
+- original indices gets lost!! 
+- need to store them in a tuple and sort based on the value
+- time complexity: O(NlogN) due to sorting
+- space complexity: O(N) due to storing the original indices in a new array
+- Best for sorted arrays aas there time complexity is O(N) and space complexity is O(1)
+*/
+function twoSumTP(nums, target) {
+    let arr = nums.map((num, i) => [num, i]);
+    
+    arr.sort((a, b) => a[0] - b[0]); // sort by value
+    
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+        let currSum = arr[left][0] + arr[right][0];
+        
+        if (currSum === target) {
+            return [arr[left][1], arr[right][1]];
+        } else if (currSum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return [];
+}
+
+//HashMap O(N):
+/*
+- use a hash map to store the numbers and their indices
+- for each number, check if the complement (target - current number) exists in the hash
+- time complexity: O(N) due to single pass through the array
+- space complexity: O(N) due to storing the numbers in the hash map
+*/
+function twoSumHM(nums, target) {
+    let hash = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        
+        if (hash.hasOwnProperty(complement)) {
+            return [hash[complement], i];
+        }
+        
+        hash[nums[i]] = i;
+    }
+    
+    return [];
+}
