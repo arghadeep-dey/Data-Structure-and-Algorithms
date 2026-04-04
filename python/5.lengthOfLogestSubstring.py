aa8a3fe6b41bba59d54a4708240b5c049bb9d2e1
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-04 23:45:43 +0530

    Length of Longest Substring in Python

---
+++ python/5.lengthOfLogestSubstring.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_map = {}
+        left = 0
+        max_length = 0
+
+        for right, char in enumerate(s):
+            if char in char_map:
+                left = max(left, char_map[char] + 1)
+            
+            char_map[char] = right
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
