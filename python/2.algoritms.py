c8facd0e14cd9f3b1b22165d9ef946eefbfdded3
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-15 12:00:00 +0530

    linear search

---
+++ python/2.algoritms.py
@@ -0,0 +1,10 @@
+#Algorithms
+
+#Searching Algorithms
+
+#1. Linear Search
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
