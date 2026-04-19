beb83e9e05496e1c3bee9fc99c8e10588de77838
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-19 12:00:00 +0530

    Insertion Sort

--- python/2.algoritms.py
+++ python/2.algoritms.py
@@ -91,4 +91,14 @@ def selection_sort(arr):
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
+# 3. Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
