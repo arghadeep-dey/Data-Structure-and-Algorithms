ac41c53cb4e27d3e041ec1e146fa6e097de8e7ff
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-26 12:00:00 +0530

    Shell Sort

--- python/2.algoritms.py
+++ python/2.algoritms.py
@@ -234,3 +234,18 @@ def bucket_sort(arr):
         sorted_arr.extend(sorted(bucket))
     return sorted_arr
 
+# 10. Shell Sort
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
