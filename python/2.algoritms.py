54ded10ad9b5ef0504375ed67491326ca2fed4c4
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-27 22:45:26 +0530

    Binary Search

--- python/2.algoritms.py
+++ python/2.algoritms.py
@@ -8,3 +8,16 @@ def linear_search(arr, target):
         if arr[i] == target:
             return i
     return -1
+
+#2. Binary Search (requires sorted array)
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
