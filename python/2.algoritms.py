2035c34d394749ae8df7ddc3ca2dd22801bd2a24
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-05-01 12:00:00 +0530

    Topological Sort

--- python/2.algoritms.py
+++ python/2.algoritms.py
@@ -596,3 +596,22 @@ def kruskal_mst(graph):
             mst.append((weight, node1, node2))
 
     return mst
+
+# 10. Topological Sort
+def topological_sort(graph):
+    visited = set()
+    stack = []
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                dfs(neighbor)
+        stack.append(node)
+
+    for node in graph:
+        if node not in visited:
+            dfs(node)
+
+    return stack[::-1]  # Return reversed stack as topological order
+
